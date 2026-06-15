b2dc4f0f417851de6403a08fa41c66825e6d0652
rosaalmeida-droid <rosa.almeida@eclisboa.net>
2026-06-15 23:07:54 +0100

    Update main.tsx

--- avaliacao-ecl/src/main.tsx
+++ avaliacao-ecl/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import './styles.css';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
