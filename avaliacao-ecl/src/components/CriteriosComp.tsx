c5af93d447f26297d87b037f06ee2a6963117920
rosaalmeida-droid <rosa.almeida@eclisboa.net>
2026-07-13 14:35:15 +0100

    Update CriteriosComp.tsx

--- avaliacao-ecl/src/components/CriteriosComp.tsx
+++ avaliacao-ecl/src/components/CriteriosComp.tsx
@@ -7,6 +7,7 @@
 
 import React, { useState } from 'react';
 import { encontrarMicro, encontrarAtitude, encontrarSubtecnica, encontrarAparelho, nomeCompetencia } from '../compatECL';
+import { getLibrary } from '../libraryService';
 
 interface Props {
   compId: string;
